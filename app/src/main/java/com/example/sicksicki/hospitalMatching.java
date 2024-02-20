42b0130143338a212b1fe6fca3b4993453e30917
SYEONIH <syeonih@naver.com>
2024-02-20 17:35:28 +0900

    create sick_sick_i

---
+++ app/src/main/java/com/example/sicksicki/hospitalMatching.java
@@ -0,0 +1,14 @@
+package com.example.sicksicki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hospitalMatching extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_matching);
+    }
+}
---
+++ app/src/main/java/com/example/sicksicki/hospitalReco.java
@@ -0,0 +1,14 @@
+package com.example.sicksicki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hospitalReco extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_reco);
+    }
+}
